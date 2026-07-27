99486322adab12d52842129cadb24116e7f17225
Marcos Rodolfo Ramos Lopez <marcosramos656@gmail.com>
2026-07-27 12:10:22 -0500

    Corregir estructura de carpetas de los mockups

---
+++ Proyecto-museo/Mockups/config.js
@@ -0,0 +1,6 @@
+// config.js - Variables globales de la aplicación del Museo del Cuervo
+const CONFIG = {
+    appsScriptUrl: "https://script.google.com/macros/s/AKfycbyzigzAp8BgtFAAayesbZm8yXVjpmuYy_0xZ9l3JatGoL4n43e4X-LFr93vcjl_wbMr/exec",
+    nombreMuseo: "Museo del Cuervo",
+    versionPrototipo: "1.2.0"
+};
